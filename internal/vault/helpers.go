ca446a79a575249f627b423ef1715218068a8725
tijmenstor <23655598+tijmenstor@users.noreply.github.com>
2025-02-19 13:59:18 +0100

    chore: Small cleanup

--- internal/vault/helpers.go
+++
@@ -1,7 +0,0 @@
-package vault
-
-import "strings"
-
-func FormatSecretName(secretName string) string {
-	return strings.ToUpper(strings.ReplaceAll(strings.ReplaceAll(secretName, " ", "_"), "-", "_"))
-}
